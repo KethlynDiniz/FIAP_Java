2a4459619fd702c19279bd5bc7e31975d80a0b88
Kethlyn Diniz <kethdiniz06@gmail.com>
2022-08-15 15:17:43 -0300

    15.08

--- src/br/com/fiap/nivelamento/PositivoNegativoNulo.java
+++ src/br/com/fiap/nivelamento/PositivoNegativoNulo.java
@@ -1,10 +1,23 @@
+//Dado um número pelo usuário, informar se ele é positivo, negativo ou nulo.
+
 package br.com.fiap.nivelamento;
+
+import java.util.Scanner;
 
 public class PositivoNegativoNulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner entrada = new Scanner(System.in);
+
+		int n;
+		System.out.println("Digite um número:");
+		n = entrada.nextInt();
+		if (n > 0)
+			System.out.println("Positivo");
+		else if (n < 0)
+			System.out.println("Negativo");
+		else
+			System.out.println("Nulo");
 	}
-
 }
